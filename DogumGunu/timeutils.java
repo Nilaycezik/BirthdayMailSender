f9d5f38aff38b9eca431f3b404c8448177e058e1
Nilay <nilaycezik@gmail.com>
2020-07-24 16:01:39 +0300

    birthday

---
+++ DogumGunu/timeutils.java
@@ -0,0 +1,23 @@
+/*   public static void counter() {
+
+        Timer tmer = new Timer();
+        TimerTask task = new TimerTask() {
+
+            int seconds = 30;
+
+
+            @Override
+            public void run() {
+                boolean a=true;
+                while (a) {
+                    System.out.println( "Kalan Zaman:" + seconds-- );
+                    if (seconds < 0) {
+                        seconds = 30;
+                    }
+                }
+            }
+
+        };
+        tmer.schedule(task,1000,1000*60*60);
+        }
+*/s
